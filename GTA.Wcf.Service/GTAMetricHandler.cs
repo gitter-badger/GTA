15017706241aa6ef0e441602c0979a55dd398a5e
kmeinster@schubergphilis.com <kmeinster@schubergphilis.com>
2015-01-19 16:55:37 +0100

    Changed the metric handler

--- GTA.Wcf.Service/GTAMetricHandler.cs
+++ GTA.Wcf.Service/GTAMetricHandler.cs
@@ -10,69 +10,27 @@ namespace GTA.Wcf.Service
 {
     public class GTAMetricHandler : ConfigurationSection
     {
-        [ConfigurationProperty("useInfluxdb", DefaultValue = "true", IsRequired = false)]
-        public Boolean useInfluxdb
-        {
-            get
-            {
-                return (Boolean)this["useInfluxdb"];
-            }
-            set
-            {
-                this["useInfluxdb"] = value;
-            }
-        }
-
-        [ConfigurationProperty("influxdburl", DefaultValue = "http://localhost:8086", IsRequired = false)]
-        public string influxdburl
-        {
-            get
-            {
-                return (string)this["influxdburl"];
-            }
-            set
-            {
-                this["influxdburl"] = value;
-            }
-        }
-
-        [ConfigurationProperty("influxdbuser", DefaultValue = "admin", IsRequired = false)]
-        public string influxdbuser
-        {
-            get
-            {
-                return (string)this["influxdbuser"];
-            }
-            set
-            {
-                this["influxdbuser"] = value;
-            }
-        }
-
-        [ConfigurationProperty("influxdbpass", DefaultValue = "secret", IsRequired = false)]
-        public string influxdbpass
-        {
-            get
-            {
-                return (string)this["influxdbpass"];
-            }
-            set
-            {
-                this["influxdbpass"] = value;
-            }
-        }
-
         public async void Write_Metric(double number1, double number2, double result, string calculation) // TO-DO Overload can be variable
         {
-
+            string myFirstpony = ConfigurationManager.AppSettings["influxdburl"];
+            string mySecondpony = ConfigurationManager.AppSettings["influxdbuser"];
+            string myThirdpony = ConfigurationManager.AppSettings["influxdbpass"];
             //var _db = new LibInfluxDB.Net.InfluxDb("http://influxdbservert:8086", "gebruikert", "wachtwoordje"); // TO-DO Use web.conf for this.
-            var _db = new LibInfluxDB.Net.InfluxDb(influxdburl, influxdbuser, influxdbpass); // TO-DO Use web.conf for this.
+            var _db = new LibInfluxDB.Net.InfluxDb(myFirstpony, mySecondpony, myThirdpony); // TO-DO Use web.conf for this.
             string dbName = "GTACalculator";
             LibInfluxDB.Net.Models.Serie serie = new LibInfluxDB.Net.Models.Serie.Builder("GTA.Wcf.Service")
                 .Columns("First", "Second", "Result", "Calculation") // TO-DO Overload can be variable
                 .Values(number1, number2, result, calculation) // TO-DO Overload can be variable
                 .Build();
             LibInfluxDB.Net.InfluxDbApiResponse writeResponse = await _db.WriteAsync(dbName, LibInfluxDB.Net.TimeUnit.Milliseconds, serie);
+        }
+
+        public class MetricParser
+        {
+            public string InputValue1 { get; set; }
+            public string InputValue2 { get; set; }
+            public string Result { get; set; }
+            public string Calculation { get; set; }
         }
     }
 }
