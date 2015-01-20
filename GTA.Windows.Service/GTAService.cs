1c07d147bba7dfb168d0a4806ac4dd81608dfd7e
Rene Mul <rmul@schubergphilis.com>
2015-01-20 14:05:45 +0100

    fabulous service with a timer

--- GTA.Windows.Service/GTAService.cs
+++ GTA.Windows.Service/GTAService.cs
@@ -8,26 +8,46 @@ using NLog;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace GTA.Windows.Service
 {
     partial class GTAService : ServiceBase
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        Timer tm = new Timer();
+
         public GTAService()
         {
+            #if DEBUG
+                logger.Debug("Service Starting, attaching debugger");
+                System.Diagnostics.Debugger.Launch();
+            #endif
             InitializeComponent();
+
+            tm.Interval = 5000;
+            tm.Elapsed += new ElapsedEventHandler(PerformOperations);
+            tm.Start();
         }
 
         protected override void OnStart(string[] args)
         {
             // TODO: Add code here to start your service.
+            
+            logger.Info("Service Started");
+
         }
 
         protected override void OnStop()
         {
             // TODO: Add code here to perform any tear-down necessary to stop your service.
+            logger.Info("Service stopped");
         }
 
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private void PerformOperations(object sener, EventArgs e)
+        {
+            logger.Info("I did it!");
+        }
+
     }
 }
