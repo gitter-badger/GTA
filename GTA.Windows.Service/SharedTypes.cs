9175ea7a29cb5e215a8a69be99e95d7bb37c5dd5
Rene Mul <rmul@schubergphilis.com>
2015-01-21 12:17:45 +0100

    Fixed the service again

---
+++ GTA.Windows.Service/SharedTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTA.Windows.Service
+{
+    public enum ScheduleTypes
+    {
+        Monthly,
+        Weekly,
+        Daily,
+        Hourly,
+        Interval
+    }
+
+    public class Schedule
+    {
+        public string Naame { get; set; }
+        public string Action { get; set; }
+        public int Interval { get; set; }
+        public ScheduleTypes ScheduleType { get; set; }
+    }
+
+    public class Schedules
+    {
+        public List<Schedule> schedules { get; set; }
+    }
+}
