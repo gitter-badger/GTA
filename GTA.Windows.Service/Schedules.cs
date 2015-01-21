9bde3b091096c9cf9dd9e2ff6a1b8accbf455664
Rene Mul <rmul@schubergphilis.com>
2015-01-21 16:26:05 +0100

    cleanup of unused items

---
+++ GTA.Windows.Service/Schedules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTA.Windows.Service
+{
+    public class Schedule
+    {
+        public string action { get; set; }
+        public int interval { get; set; }
+    }
+
+    public class Schedules
+    {
+        public List<Schedule> schedules { get; set; }
+    }
+}
